09312e6f0c0af00c9c8c07d9747c9b3aea256c96
devlinduldulao <webmasterdevlin@gmail.com>
2022-10-18 09:58:10 +0200

    update

--- src/pages/Features.vue
+++ src/pages/Features.vue
@@ -1,22 +1,8 @@
-<script lang="ts">
+<script setup lang="ts">
 import Dashboard from "../layouts/Dashboard.vue";
-import { storeToRefs } from "pinia";
-import { defineComponent } from "vue";
 import { useCounterStore } from "../stores/counter";
 
-export default defineComponent({
-  name: "Home",
-  components: { Dashboard },
-  setup() {
-    // store getters
-    const { doubleCount } = storeToRefs(useCounterStore());
-
-    // store actions
-    const { increment, reset } = useCounterStore();
-
-    return { doubleCount, increment, reset };
-  },
-});
+const storeCounter = useCounterStore();
 </script>
 <template>
   <Dashboard>
@@ -24,12 +10,15 @@ export default defineComponent({
       <div class="container" style="heigth: 100vh; width: 100%">
         <section>
           <h1>
-            Count : <span style="color: purple">{{ doubleCount }}</span>
+            Count :
+            <span style="color: purple">{{ storeCounter.doubleCount }}</span>
           </h1>
-          <button class="btn btn-primary" @click="increment">
+          <button class="btn btn-primary" @click="storeCounter.increment">
             Increment by 2
           </button>
-          <button class="btn btn-danger" @click="reset">reset</button>
+          <button class="btn btn-danger" @click="storeCounter.reset">
+            reset
+          </button>
         </section>
       </div>
     </div>
--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,34 +1,23 @@
-<script lang="ts">
+<script setup lang="ts">
 import Dashboard from "../layouts/Dashboard.vue";
 import { storeToRefs } from "pinia";
 import { defineComponent, onMounted } from "vue";
 import { usePostStore } from "../stores/post";
 
-export default defineComponent({
-  name: "Home",
-  components: { Dashboard },
-  setup() {
-    // store getters
-    const { posts, loading, doloremContent } = storeToRefs(usePostStore());
+const storePost = usePostStore();
 
-    // store actions
-    const { fetchPosts } = usePostStore();
-
-    onMounted(async () => {
-      await fetchPosts();
-    });
-
-    return { posts, loading, doloremContent };
-  },
+onMounted(async () => {
+  await storePost.fetchPosts();
 });
+
 </script>
 <template>
   <Dashboard>
     <div class="container p-5" style="heigth: 100%; width: 100%">
       <section>
-        <h2 v-if="loading">Loading..</h2>
+        <h2 v-if="storePost.loading">Loading..</h2>
         <div v-else>
-          <div v-for="post in doloremContent" :key="post.id">
+          <div v-for="post in storePost.doloremContent" :key="post.id">
             <h3>{{ post.title }}</h3>
             <p>{{ post.body }}</p>
           </div>
