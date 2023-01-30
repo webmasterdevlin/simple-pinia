31e0b828ba23d0156f71d7450b02aaad1b593d94
devlin <webmasterdevlin@gmail.com>
2023-01-30 02:39:49 +0100

    update

--- src/layouts/Dashboard.vue
+++ src/layouts/Dashboard.vue
@@ -3,7 +3,6 @@ import LeftSideBarMenu from "../components/LeftSideBarMenu.vue";
 </script>
 
 <template>
-  <!-- converting style to Tailwind class -->
   <div class="bg-yellow-100 w-full h-auto flex flex-row justify-start">
     <left-side-bar-menu />
     <slot></slot>
